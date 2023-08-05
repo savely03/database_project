e79fdb78bb9b73650c15211ff64e242dcbdfdff3
Savely Domnikov <savely.domnikov@yandex.ru>
2023-08-05 17:19:52 +0300

    Added some changes

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS CLIENTS (
     id BIGSERIAL PRIMARY KEY,
-    full_name varchar(50) NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
     date_birthday DATE NOT NULL,
     gender INT CHECK ( gender >= 1 and gender <= 2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CARS (
     id BIGSERIAL PRIMARY KEY,
-    model varchar(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
     color VARCHAR(20) NOT NULL,
     engine_capacity DECIMAL NOT NULL,
     release_date DATE NOT NULL,

e51aa6b816a100f3fda496a9425114a97a0224e1
Savely Domnikov <savely.domnikov@yandex.ru>
2023-08-18 22:55:29 +0300

    Completed refactoring in dml

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -1,7 +1,7 @@
 ---Какой клиент сколько машин заказал
 SELECT c.id        as client_id,
        c.full_name as full_name,
-       COUNT(*)    as count_cars
+       COUNT(1)    as count_cars
 FROM clients c
          JOIN orders o
               on c.id = o.client_id
@@ -11,7 +11,7 @@ ORDER BY c.id;
 
 
 ---Как много заказали красных машин
-SELECT COUNT(*) as count_cars
+SELECT COUNT(1) as count_cars
 FROM cars c
          JOIN orders o
               ON c.id = o.car_id
@@ -24,14 +24,14 @@ FROM clients c
          JOIN orders o
               ON c.id = o.client_id
 GROUP BY c.id
-ORDER BY count(*) DESC
+ORDER BY count(1) DESC
 LIMIT 1;
 
 
 ---ФИО клиента, который заказал больше всего машин, с учётом, что таких клиентов может быть несколько
 SELECT a.full_name as full_name
 FROM (SELECT c.full_name                                as full_name,
-             dense_rank() over (ORDER BY count(*) DESC) as rnk
+             dense_rank() over (ORDER BY count(1) DESC) as rnk
       FROM clients c
                JOIN orders o
                     ON c.id = o.client_id
@@ -43,14 +43,14 @@ WHERE a.rnk = 1;
 SELECT extract(MONTH FROM o.order_date) as month
 FROM orders o
 GROUP BY month
-ORDER BY count(*) DESC
+ORDER BY count(1) DESC
 LIMIT 1;
 
 
 ---В каком месяце больше всего заказывают машин, с учетом, что таких месяцев может быть несколько
 SELECT a.month as month
 FROM (SELECT extract(MONTH FROM o.order_date)            as month,
-             dense_rank() over (order by count(*) desc ) as rnk
+             dense_rank() over (order by count(1) desc ) as rnk
       FROM orders o
       group by month) as a
 WHERE a.rnk = 1;
